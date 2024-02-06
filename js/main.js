998620e8a5c95bd7f232bd3e60ed0f237064d031
Aleksey Borisov <loja.bobaev2000@yandex.ru>
2024-02-06 21:39:38 +0300

    home: создание комментариев

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import {names, comments, descriptions} from './data.js';
+import data from './data.js';
 
+// Получение случайного положительного целого числа
 const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -18,10 +19,11 @@ function getRandomArrayElement(array) {
 function createMessage() {
   const messages = [];
   for (let i = 0; i < getRandomPositiveInteger(1, 2); i++) {
-    messages.push(getRandomArrayElement(comments));
+    messages.push(getRandomArrayElement(data.comments));
   }
   return messages.join(' ');
 }
+
 //console.log(createMessage());
 
 //Создание объекта комментария
@@ -30,7 +32,30 @@ function createComment() {
     id: getRandomPositiveInteger(1, 25),
     avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
     message: createMessage(),
-    name: getRandomArrayElement(names),
+    name: getRandomArrayElement(data.names),
   };
 }
-console.log(createComment());
+
+// console.log(createComment(25));
+
+// Создание объекта фотографии
+function createPhoto(id) {
+  return {
+    id: id,
+    photo: `photos/${id}.jpg`,
+    description: getRandomArrayElement(data.descriptions),
+    likes: getRandomPositiveInteger(15, 100),
+    comments: Array.from({length: getRandomPositiveInteger(1, 25)}, createComment)
+  };
+}
+
+// console.log( createPhoto(1) );
+
+// Создание набора фотографий
+function createPhotos(count) {
+
+  return Array.from({length: count}).map((value, index) => createPhoto(index + 1));
+
+}
+
+// console.log( createPhotos(2) );
