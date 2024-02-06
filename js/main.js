3a690f464bd8556599265e18a1d067b8b1dbe518
Aleksey Borisov <loja.bobaev2000@yandex.ru>
2024-02-06 20:47:25 +0300

    home: исправление ошибки в работе функции

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import {names, comments, descriptions} from './data.js';
 
 const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
 
   const res = Math.random() * (upper - lower + 1) + lower;
 
