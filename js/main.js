6523c82e6cf785b889970e2e81c289475c8c6882
Aleksey Borisov <loja.bobaev2000@yandex.ru>
2024-02-06 15:29:45 +0300

    class: Подготовка

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 
+const checkStringLength = (string, length) => string.length <= length;
+
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.min(Math.abs(a), Math.abs(b)));
+
+  const res = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(res);
+};
