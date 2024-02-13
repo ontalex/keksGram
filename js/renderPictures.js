91652f6b61db21dfe49a5fd434b49ffe087a6881
Grachev Maxim <maxoowww@gmail.com>
2024-02-13 13:41:16 +0300

    class: добавил отрисовку фотографий

---
+++ js/renderPictures.js
@@ -0,0 +1,24 @@
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const container = document.querySelector('.pictures');
+
+//Создание элемента фотографии
+const createPictureElement = (picture) =>{
+  const {url,likes,comments} = picture;
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  return pictureElement;
+};
+
+//Отображение фотографий
+const renderPictures = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const pictureElement = createPictureElement(picture);
+    fragment.append(pictureElement);
+  });
+  container.append(fragment);
+};
+
+export {renderPictures};
