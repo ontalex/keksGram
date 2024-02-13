81d23c31318df65512644bf7c9b593a0a2d15dd3
Aleksey Borisov <loja.bobaev2000@yandex.ru>
2024-02-13 21:33:26 +0300

    home: форматирование

--- js/placeholder.js
+++ js/placeholder.js
@@ -1,41 +1,41 @@
 import { names, descriptions, comments } from './data.js';
-import {getRandomPositiveInteger, getRandomArrayElement, getID} from './utils.js';
+import { getRandomPositiveInteger, getRandomArrayElement, getID } from './utils.js';
 
 //Случайные комментарии к фотографиям
 function createMessage() {
-  const messages = [];
-  for (let i = 0; i < getRandomPositiveInteger(1, 2); i++) {
-    messages.push(getRandomArrayElement(comments));
-  }
-  return messages.join(' ');
+    const messages = [];
+    for (let i = 0; i < getRandomPositiveInteger(1, 2); i++) {
+        messages.push(getRandomArrayElement(comments));
+    }
+    return messages.join(' ');
 }
 
 //Создание объекта комментария
 function createComment() {
-  return {
-    id: getID(),
-    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
-    message: createMessage(),
-    name: getRandomArrayElement(names),
-  };
+    return {
+        id: getID(),
+        avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+        message: createMessage(),
+        name: getRandomArrayElement(names),
+    };
 }
 
 // Создание объекта фотографии
 function createPhoto(id) {
-  return {
-    id: id,
-    url: `photos/${id}.jpg`,
-    description: getRandomArrayElement(descriptions),
-    likes: getRandomPositiveInteger(15, 200),
-    comments: Array.from({length: getRandomPositiveInteger(1, 25)}, createComment)
-  };
+    return {
+        id: id,
+        url: `photos/${id}.jpg`,
+        description: getRandomArrayElement(descriptions),
+        likes: getRandomPositiveInteger(15, 200),
+        comments: Array.from({ length: getRandomPositiveInteger(1, 25) }, createComment)
+    };
 }
 
 // Создание набора фотографий
 function createPhotos() {
 
-  return Array.from({length: 25}, (_, index) => createPhoto(index + 1));
+    return Array.from({ length: 25 }, (_, index) => createPhoto(index + 1));
 
 }
 
-export {createPhotos};
+export { createPhotos };
--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,18 @@
 // Получение случайного положительного целого числа
 const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+    const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+    const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
 
-  const res = Math.random() * (upper - lower + 1) + lower;
+    const res = Math.random() * (upper - lower + 1) + lower;
 
-  return Math.floor(res);
+    return Math.floor(res);
 };
 
 //Получение случайного элемента массива
 function getRandomArrayElement(array) {
-  return array[getRandomPositiveInteger(0, array.length - 1)];
+    return array[getRandomPositiveInteger(0, array.length - 1)];
 }
 
 const getID = () => Date.now() + (Math.random() * 100000).toFixed(0);
 
-export {getRandomPositiveInteger, getRandomArrayElement, getID};
+export { getRandomPositiveInteger, getRandomArrayElement, getID };
