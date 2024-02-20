6250bcb14705c46f7e5015878a6940f1e4234692
Grachev <maxoowww@gmail.com>
2024-02-20 15:49:58 +0300

    class: создал слайдер

---
+++ js/effect.js
@@ -0,0 +1,108 @@
+
+const image = document.querySelector('.img-upload__preview img');
+const effectLevel = document.querySelector('.effect-level__value');
+const form = document.querySelector('.img-upload__form');
+const sliderElement = document.querySelector('.effect-level__slider');
+const EFFECTS = [
+  {
+    name: 'none',
+    min: 0,
+    max: 100,
+    step: 1,
+  },
+  {
+    name: 'chrome',
+    style: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  {
+    name: 'sepia',
+    style: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  {
+    name: 'marvin',
+    style: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%',
+  },
+  {
+    name: 'phobos',
+    style: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    unit: 'px',
+  },
+  {
+    name: 'heat',
+    style: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    unit: ''
+  },
+];
+
+
+const DEFAULT_EFFECT = EFFECTS[0];
+let chosenEffect = DEFAULT_EFFECT;
+const isDefault = () => chosenEffect === DEFAULT_EFFECT;
+
+const updateSlider = () => {
+  sliderElement.classList.remove('hidden');
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: chosenEffect.min,
+      max: chosenEffect.max,
+    },
+    step: chosenEffect.step,
+    start: chosenEffect.max,
+  });
+
+  if (isDefault()) {
+    sliderElement.classList.add('hidden');
+  }
+};
+
+const onFormChange = (evt) => {
+  if (!evt.target.classList.contains('effects__radio')) {
+    return;
+  }
+  chosenEffect = EFFECTS.find((effect) => effect.name === evt.target.value);
+  updateSlider();
+};
+
+const onSliderUpdate = () => {
+  image.style.filter = 'none';
+  image.className = '';
+  effectLevel.value = '';
+  if (isDefault()) {
+    return;
+  }
+  const sliderValue = sliderElement.noUiSlider.get();
+  image.style.filter = `${chosenEffect.style}(${sliderValue}${chosenEffect.unit})`;
+  image.classList.add(`effects__preview--${chosenEffect.name}`);
+  effectLevel.value = sliderValue;
+};
+
+
+noUiSlider.create(sliderElement, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
+updateSlider();
+form.addEventListener('change', onFormChange);
+sliderElement.noUiSlider.on('update',onSliderUpdate);
