a263d49bd20516834e86dfb4077a0045ffe59803
Grachev <maxoowww@gmail.com>
2024-02-20 15:53:25 +0300

    class: исправил слайдер

--- js/effect.js
+++ js/effect.js
@@ -1,4 +1,3 @@
-
 const image = document.querySelector('.img-upload__preview img');
 const effectLevel = document.querySelector('.effect-level__value');
 const form = document.querySelector('.img-upload__form');
@@ -48,7 +47,7 @@ const EFFECTS = [
     min: 1,
     max: 3,
     step: 0.1,
-    unit: ''
+    unit: '',
   },
 ];
 
@@ -94,15 +93,16 @@ const onSliderUpdate = () => {
   effectLevel.value = sliderValue;
 };
 
-
 noUiSlider.create(sliderElement, {
-    start: [20, 80],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': 100
-    }
+  range: {
+    min: DEFAULT_EFFECT.min,
+    max: DEFAULT_EFFECT.max,
+  },
+  start: DEFAULT_EFFECT.max,
+  step: DEFAULT_EFFECT.step,
+  connect: 'lower',
 });
 updateSlider();
 form.addEventListener('change', onFormChange);
 sliderElement.noUiSlider.on('update',onSliderUpdate);
+
