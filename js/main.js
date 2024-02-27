5759662bfa6b069811213e7fc8b0f5a6a9e41780
Aleksey Borisov <loja.bobaev2000@yandex.ru>
2024-02-27 21:33:01 +0300

    home: чистка

--- js/main.js
+++ js/main.js
@@ -1,39 +1,3 @@
 import { createPhotos } from './placeholder.js';
 import { renderPictures } from './renderPictures.js';
-import { hideModal, setOnFormSubmit } from './form.js';
-import { sendData } from './api.js';
-
-let success = document.getElementById('success');
-let error = document.getElementById('error');
-
-const onSendDataSuccess = () => {
-  //закрыть окно формы, показать сообщение c успешной отправкой
-  const popupNode = success.content.querySelector('.success').cloneNode(true);
-
-  popupNode
-    .querySelector('.success__button')
-    .addEventListener('click', () => popupNode.remove());
-
-  document.body.after(popupNode);
-
-  hideModal();
-};
-
-const onSendDataError = () => {
-  // показать сообщение с ошибкой
-  const popupNode = error.content.querySelector('.error').cloneNode(true);
-
-  popupNode
-    .querySelector('.error__button')
-    .addEventListener('click', () => popupNode.remove());
-
-  document.body.after(popupNode);
-
-  hideModal();
-};
-
-setOnFormSubmit(async (data) => {
-  await sendData(onSendDataSuccess, onSendDataError, data);
-});
-
 renderPictures(createPhotos());
